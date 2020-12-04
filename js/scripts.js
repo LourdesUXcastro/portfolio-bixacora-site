46b9846df10d0a0e7a0b5edf6e3826748f30d0e2
LourdesUXcastro <74964846+LourdesUXcastro@users.noreply.github.com>
2020-12-04 19:59:29 +0100

    JavaScript 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
-console.log("Welcome to my site!");
+console.log("Welcome to my Bixacora!");
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
     x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
+  }
+}
+const parallaxEls = document.querySelectorAll("[data-speed]");
+
+window.addEventListener("scroll", scrollHandler);
+
+function scrollHandler() {
+  for (const parallaxEl of parallaxEls) {
+    const direction = parallaxEl.dataset.direction == "up" ? "-" : "";
+    const transformY = this.pageYOffset * parallaxEl.dataset.speed;
   }
 }
